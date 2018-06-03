659fe19928fd0d63206ca5d15f5a372de78bd1a9
unknown <test@adami3.com>
2018-06-04 01:21:42 +0200

    first commit

---
+++ routes/tracks.js
@@ -0,0 +1,186 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : '',
+    user     : '',
+    password : '',
+    database : ''
+});
+
+
+connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ...");
+    } else {
+        console.log("Error connecting database ...");
+    }
+});
+
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+// });
+
+exports.tracks = function(req,res) {
+    connection.query('SELECT * FROM tracks ORDER BY id DESC LIMIT 20', function (error, results, fields) {
+        if (error) {
+            console.log('error');
+            res.send({
+                'code': 400,
+                'error': 'error occured'
+            })
+        } else {
+            res.send({
+                'code': 200,
+                'success': 'the tracklist has been found',
+                results
+            })
+        }
+    })
+}
+
+exports.allTracks = function(req,res) {
+    connection.query('SELECT * FROM tracks ORDER BY id DESC LIMIT 100', function (error, results, fields) {
+        if (error) {
+            console.log('error');
+            res.send({
+                'code': 400,
+                'error': 'error occured'
+            })
+        } else {
+            res.send({
+                'code': 200,
+                'success': 'the tracklist has been found',
+                results
+            })
+        }
+    })
+}
+
+exports.publish = function (req, res, next) {
+    console.log("req");
+    // console.log(req.file);
+    if (!req.file) {
+        return res.send('no files were uploaded');
+    } else {
+        console.log("request");
+        console.log(req.body);
+        let path = {
+            name: req.file.filename,
+            userName: req.body.userName
+        };
+        connection.query('INSERT INTO tracks SET ?', path, function (error, results, fields) {
+            if (error) {
+                console.log('error');
+                res.send({
+                    'code': 400,
+                    'error': 'error occured'
+                })
+            } else {
+                console.log('success');
+                res.send({
+                    'code': 200,
+                    'success': 'successful'
+                });
+            }
+        })
+    }
+}
+
+exports.register = function(req,res){
+    console.log("req",req.body);
+    var today = new Date();
+    var users={
+        "name":req.body.name,
+        "email":req.body.email,
+        "password":req.body.password,
+        "creationDate":today,
+    }
+    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred",error);
+            res.send({
+                "code":400,
+                "failed":"error ocurred"
+            })
+        }else{
+            res.redirect("http://localhost:8080/login");
+        }
+    });
+}
+
+exports.login = function(req,res){
+    var name = req.body.name;
+    var password = req.body.password;
+    connection.query('SELECT * FROM users WHERE name = ? AND password = ?', [name, password], function (error, results, fields) {
+        console.log("req");
+        console.log(req.session);
+        if (error) {
+            console.log("error ocurred",error);
+            res.send({
+                "code":400,
+                "failed":"error ocurred"
+            })
+        }else{
+            if(results.length >0){
+                if(results[0].password == password){
+                    results = {
+                        userName: results[0].name,
+                        id: results[0].id
+                    };
+                    res.send({
+                        "code":200,
+                        "success":"login successful",
+                        results
+                    })
+                }
+                else{
+                    res.send({
+                        "code":204,
+                        "error":"Email and password does not match"
+                    });
+                }
+            }
+            else{
+                res.send({
+                    "code":204,
+                    "error":"Email does not exist"
+                });
+            }
+        }
+    });
+}
+
+exports.user = ((req, res) => {
+    console.log("req");
+    console.log(req.body);
+    var id = req.body.id;
+    connection.query('SELECT name FROM users WHERE id = ?', [id], (error, results, fields) => {
+        if (error) {
+            res.send({
+                "code": 400,
+                "error": "bad request"
+            });
+        } else {
+            res.send({
+                results
+            })
+        }
+    })
+});
+
+exports.delete = ((req, res) => {
+    var id = req.body.id;
+    connection.query('DELETE FROM tracks WHERE id = ?', [id], (error, results, fields) => {
+        if (error) {
+            res.send({
+                "code": 400,
+                "error": "Bad Request"
+            });
+        } else {
+            res.send({
+                "code": 200,
+                "sucess": "the track has been deleted"
+            })
+        }
+    })
+});
---
+++ server.js
@@ -0,0 +1,59 @@
+
+var express = require('express');
+var tracksroutes = require('./routes/tracks.js');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+// var session = require('express-session');
+var multer  = require('multer');
+
+var app = express();
+
+// var sess = {
+//   secret: 'keyboard cat',
+//   cookie: {
+//       maxAge: 600000
+//   }
+// };
+
+// config simple du C.O.R.S
+app.use(cors({
+  origin: [
+    "http://212.47.243.197:8080",
+    "http://212.47.243.197:8081",
+    "http://212.47.243.197:8082",
+    "http://212.47.243.197:8083",
+  ], // domaines validés
+  methods: ["GET", "DELETE", "PATCH", "POST", "PUT"], // verbes http validés
+  credentials: true // autorise set cookie
+}));
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './static/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "_" + file.originalname)
+    }
+})
+
+var upload = multer({ storage: storage })
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(session(sess));
+
+app.get('/', tracksroutes.tracks);
+app.get('/library', tracksroutes.allTracks);
+
+
+// app.post('/publish', fileUpload(), tracksroutes.publish);
+app.post('/user', tracksroutes.user);
+app.post('/publish', upload.single("sound"), tracksroutes.publish);
+app.post('/login', tracksroutes.login);
+app.post('/register', tracksroutes.register);
+
+app.delete('/delete', tracksroutes.delete);
+
+
+
+app.listen(5000);
